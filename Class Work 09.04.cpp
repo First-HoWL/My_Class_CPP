6c9bc3c25045e37772c1828fcfd96eee888214c9
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2024-09-04 19:19:23 +0300

    Update Class Work 09.04.cpp

--- Class Work 09.04.cpp
+++ Class Work 09.04.cpp
@@ -3,29 +3,52 @@ using namespace std;
 
 int main()
 {
-
+	/*
 	int a;
 	cout << "Your age:\n";
 	cin >> a;
-
+	while (true)
+	{
 	if (a <= 0 || a >= 120) {
-		cout << "lie";
+		cout << "lie\n";
+		cout << "Your age:\n";
+		cin >> a;
 	}
 	else if (a < 3) {
-		cout << "ti nemovlya";
+		cout << "ti nemovlya"; break;
 	}
 	else if (a < 10) {
-		cout << "ti ditina";
+		cout << "ti ditina"; break;
 	}
 	else if (a < 18) {
-		cout << "ti ne soverchenoletnii";
+		cout << "ti ne soverchenoletnii"; break;
 	}
 	else if (a < 60) {
-		cout << "ti soverchenoletnii";
+		cout << "ti soverchenoletnii"; break;
 	}
 	else if (a < 120) {
-		cout << "ti starik";
+		cout << "ti starik"; break;
 	}
+	
 	cout << "\n";
+	}
+	*/
+	int a;
+	cout << "Your year:\n";
+	cin >> a;
+
+	if (a % 400 == 0) {
+		cout << "visocosnii";
+	}
+	else if (a % 100 == 0) {
+		cout << "ne visocosnii";
+	}
+	else if (a % 4 == 0) {
+		cout << "visocosnii";
+	}
+	else {
+		cout << "ne visocosnii";
+	}
+
 	return 0;
 }
