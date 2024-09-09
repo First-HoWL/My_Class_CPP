93bd961b5e1059e78625fd34aff8ce63ed7ebc2a
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2024-09-09 20:09:13 +0300

    Update Class Work 09.04.cpp

--- Class Work 09.04.cpp
+++ Class Work 09.04.cpp
@@ -8,41 +8,73 @@ int randint(int min, int max) {
 
 int main()
 {
-	int a, b, t = 0, d, e;
+	/*
+	int a;
+	cout << "Your age:\n";
+	cin >> a;
+	int a, b;
 	srand(time(NULL));
-	d = time(NULL);
 	a = randint(0, 200);
 	cout << a << "\n";
 	cout << "Vgadai chislo:\n";
 	cin >> b;
 	while (true)
 	{
-		t++;
-		if (b == 0) {
-			break;
-		}
+	if (a <= 0 || a >= 120) {
+		cout << "lie\n";
+		cout << "Your age:\n";
+		cin >> a;
+	}
+	else if (a < 3) {
+		cout << "ti nemovlya"; break;
+	}
+	else if (a < 10) {
+		cout << "ti ditina"; break;
+	}
+	else if (a < 18) {
+		cout << "ti ne soverchenoletnii"; break;
+	}
+	else if (a < 60) {
+		cout << "ti soverchenoletnii"; break;
+	}
+	else if (a < 120) {
+		cout << "ti starik"; break;
+	}
+	
+	cout << "\n";
+	}
+	*/
+	int a;
+	cout << "Your year:\n";
+	cin >> a;
+
+	if (a % 400 == 0) {
+		cout << "visocosnii";
+	}
+	else if (a % 100 == 0) {
+		cout << "ne visocosnii";
+	}
+	else if (a % 4 == 0) {
+		cout << "visocosnii";
+	}
+	else {
+		cout << "ne visocosnii";
 		if (a == b) {
-			cout << "you win!";
+			cout << "you win! Number is: " << a;
 			break;
 		}
 		else
 		{
 			cout << "Nevirno,";
-			if (a - b >= -20 && a - b <= 20) {
-				cout << "garacho";
+			if (a > b) {
+				cout << "zagadone chislo bilche";
 			}
-			else if (a - b >= -50 && a - b <= 50) {
-				cout << "teplo";
-			}
-			else if (a - b >= -100 && a - b <= 100) {
-				cout << "holodno";
+			else if (a < b) {
+				cout << "zagadone chislo menche";
 			}
 			cout << "\n";
 			cin >> b;
 		}
 	}
-	e = time(NULL);
-	cout << "Number is: " << a << ";\n" << t << " tries\n" << (e - d) << " seconds";
 
 	return 0;
-}
