ba60348986def7870750106148f0015044f9ae36
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2024-09-09 20:07:48 +0300

    Update Class Work 09.04.cpp

--- Class Work 09.04.cpp
+++ Class Work 09.04.cpp
@@ -8,31 +8,41 @@ int randint(int min, int max) {
 
 int main()
 {
-	int a, b;
+	int a, b, t = 0, d, e;
 	srand(time(NULL));
+	d = time(NULL);
 	a = randint(0, 200);
 	cout << a << "\n";
 	cout << "Vgadai chislo:\n";
 	cin >> b;
 	while (true)
 	{
+		t++;
+		if (b == 0) {
+			break;
+		}
 		if (a == b) {
-			cout << "you win! Number is: " << a;
+			cout << "you win!";
 			break;
 		}
 		else
 		{
 			cout << "Nevirno,";
-			if (a > b) {
-				cout << "zagadone chislo bilche";
+			if (a - b >= -20 && a - b <= 20) {
+				cout << "garacho";
 			}
-			else if (a < b) {
-				cout << "zagadone chislo menche";
+			else if (a - b >= -50 && a - b <= 50) {
+				cout << "teplo";
+			}
+			else if (a - b >= -100 && a - b <= 100) {
+				cout << "holodno";
 			}
 			cout << "\n";
 			cin >> b;
 		}
 	}
+	e = time(NULL);
+	cout << "Number is: " << a << ";\n" << t << " tries\n" << (e - d) << " seconds";
 
 	return 0;
 }
