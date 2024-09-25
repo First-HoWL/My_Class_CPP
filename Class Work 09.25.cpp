dedc1e3a4e33fea4affd80ccd5840f9b06f133b1
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2024-09-25 19:15:40 +0300

    Create Class Work 09.25.cpp

---
+++ Class Work 09.25.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <time.h>
+#include <windows.h>
+using namespace std;
+
+void SetColor(int textColor, int bgColor)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole,
+		(bgColor << 4) | textColor);
+}
+
+void bubble_sort(int array[], const int len) {
+	bool is_changed = true;
+	int a;
+	for (int n = 0; n < len - 1; n++)
+	{
+		is_changed = false;
+		for (int i = 0; i < len - n - 1; i++) {
+			if (array[i] < array[i + 1]) {
+				swap(array[i], array[i + 1]);
+				is_changed = true;
+			}
+		}
+		if (is_changed == false)
+		{
+			break;
+		}
+	}
+}
+
+void cout_masiv(int array[], int len) {
+	for (int i = 0; i < len; i++)
+	{
+		cout << array[i] << ", ";
+	}
+	cout << "\b\b.";
+}
+
+int randint(int min, int max) {
+	return(rand() % (max - min + 1) + min);
+}
+
+int summa(int array[], int len) {
+	int sum = 0;
+	for (int i = 0; i < len; i++)
+	{
+		sum += array[i];
+	}
+	return sum;
+}
+
+int kilkist_parnih(int array[], int len) {
+	int kilkist = 0;
+	for (int i = 0; i < len; i++) {
+		if (array[i] % 2 == 0) {
+			kilkist++;
+		}
+	}
+	return kilkist;
+}
+
+int main()
+{
+	srand(time(0));
+	const int len = 10;
+	int array[len];
+	for (int i = 0; i < len; i++) {
+		array[i] = randint(-50, 50);
+	}
+
+	cout_masiv(array, len);
+	cout << "\nsumma: " << summa(array, len) << endl;
+	bubble_sort(array, len);
+
+	cout << "min: " << array[(len - 1)] << endl;
+	cout << "max: " << array[0] << endl;
+	cout << "parnih: " << kilkist_parnih(array, len) << endl;
+
+	return 0;
+}
