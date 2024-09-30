5f92eb1636e6d78fa5d6debefd97360e6d3a2e94
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2024-09-30 19:19:22 +0300

    Update Class Work 09.30.cpp

--- Class Work 09.30.cpp
+++ Class Work 09.30.cpp
@@ -15,6 +15,15 @@ int randint(int min, int max) {
 	return(rand() % (max - min + 1) + min);
 }
 
+float seredne_arefmetichne(int array[], int len) {
+	float seredne = 0;
+	for (int i = 0; i < len; i++) {
+		seredne += array[i];
+	}
+	seredne = seredne / len;
+	return seredne;
+}
+
 int main()
 {
 	srand(time(0));
@@ -23,13 +32,20 @@ int main()
 
 	cout << "Enter array size: ";
 	cin >> size;
-	
+
 	int* array = new int[size];
-	
+
 	for (int i = 0; i < size; i++)
 		array[i] = randint(-50, 50);
 
 	cout_masiv(array, size);
+
+	cout << "\nseredne arefmetichne: " << seredne_arefmetichne(array, size) << endl;
+
 	delete[] array;
 	return 0;
 }
+
+
+
+
