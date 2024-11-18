fe7cbde52ce04972ce986796fbb4c5d9a6e8d103
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2024-11-18 18:52:37 +0200

    Update Class Work 11.18.cpp

--- Class Work 11.18.cpp
+++ Class Work 11.18.cpp
@@ -29,14 +29,38 @@ int main()
 	file << "Hello world!\n";
 	file.close();
 	*/
-	file.open(FILE_PATH, ios::in);
-	while (file) {
-		string text;
-		//file >> text;
-		getline(file, text);
-		cout << text << endl;
+
+	while (true) {
+		cout << "1. New file 2. Cout file 3. Add something to the file 0. Exit \n >> ";
+		int vibor;
+		cin >> vibor;
+		system("cls");
+		if (vibor == 0)
+			break;
+		else if (vibor == 1) {
+			file.open(FILE_PATH, ios::out);
+			file.close();
+		}
+		else if (vibor == 2) {
+			file.open(FILE_PATH, ios::in);
+			while (file) {
+				string text;
+				getline(file, text);
+				cout << text << endl;
+			}
+			file.close();
+		}
+		else if (vibor == 3) {
+			string text;
+			file.open(FILE_PATH, ios::app);
+			cout << " >> ";
+			cin >> text;
+			file << text << endl;
+			file.close();
+		}
 	}
-	file.close();
+
+	
 
 	return 0;
 }
