a230743411f36c51344b3ba8e11184f821a9a8a0
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2024-11-18 20:07:10 +0200

    Update Class Work 11.18.cpp

--- Class Work 11.18.cpp
+++ Class Work 11.18.cpp
@@ -23,7 +23,7 @@ int randint(int min, int max) {
 int main()
 {
 	srand(time(0));
-	fstream file;
+	fstream file, file2;
 	/*
 	file.open(FILE_PATH, ios::app);
 	file << "Hello world!\n";
@@ -31,7 +31,7 @@ int main()
 	*/
 
 	while (true) {
-		cout << "1. New file 2. Cout file 3. Add something to the file 0. Exit \n >> ";
+		cout << "1. New file 2. Cout file 3. Add smth to the file 4. Find longest string 5. Copy file 6. File long words 7. loking for char 0. Exit \n >> ";
 		int vibor;
 		cin >> vibor;
 		system("cls");
@@ -57,6 +57,71 @@ int main()
 			cin >> text;
 			file << text << endl;
 			file.close();
+		}
+		else if (vibor == 4) {
+			file.open(FILE_PATH, ios::in);
+			string text, correct_text;
+			int size = 0;
+			while (file) {
+				getline(file, text);
+				if (text.length() > size){
+					size = text.length();
+					correct_text = text;
+				}
+								
+			}
+			cout << correct_text << endl << size << "characters!" << endl;
+			file.close();
+		}
+		else if (vibor == 5) {
+			cout << "name new file: ";
+			char FILE_PATH_NEW[30];
+			cin >> FILE_PATH_NEW;
+
+			file2.open(FILE_PATH_NEW, ios::out);
+			file.open(FILE_PATH, ios::in);
+			string text;
+			while (file) {
+				getline(file, text);
+				file2 << text << endl;
+			}
+			file.close();
+			file2.close();
+			cout << "correct!" << endl;
+		}
+		else if (vibor == 6) {
+			cout << "name new file: ";
+			char FILE_PATH_NEW[30];
+			cin >> FILE_PATH_NEW;
+
+			file2.open(FILE_PATH_NEW, ios::out);
+			file.open(FILE_PATH, ios::in);
+			string text;
+			while (file) {
+				getline(file, text);
+				if (text.length() > 7)
+					file2 << text << endl;
+			}
+			file.close();
+			file2.close();
+			cout << "correct!" << endl;
+		}
+		else if (vibor == 7) {
+			cout << "character: ";
+			char character;
+			cin >> character;
+			file.open(FILE_PATH, ios::in);
+			string text;
+			int count = 0;
+			while (file) {
+				file >> text;
+				if (character == text[0])
+					count++;
+			}
+			if (character == text[0])
+				count--;
+			file.close();
+			cout << count << " words!" << endl;
 		}
 	}
 
